1460fff98b9104196a88d431bb991d7456c39d27
Ademdkr <adem.dokur@outlook.de>
2025-05-16 00:29:30 +0200

    CI: Headless Chrome für GitHub Actions

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,8 +1,35 @@
+// frontend/karma.conf.js
+
 module.exports = function (config) {
   config.set({
-    // … andere Einstellungen …
-
-    // Nutze unseren eigenen Headless-Launcher
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // lässt das Spec-Runner-Output im Browser stehen
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    // Hier unsere Anpassung für CI:
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
@@ -14,13 +41,7 @@ module.exports = function (config) {
         ]
       }
     },
-
-    // Browser auf GitHub Actions
-    browsers: ['ChromeHeadlessCI'],
-
-    // Einmal durchlaufen, dann beenden
     singleRun: true,
-
-    // … weitere Einstellungen …
+    restartOnFileChange: false
   });
 };
